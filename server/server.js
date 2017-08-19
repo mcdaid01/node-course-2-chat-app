da05cd7d363268b60353d7f1bba69103c374b4fe
Mike McDaid <mcdaid01@gmail.com>
2017-08-19 13:34:17 +0100

    initial commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const publicPath = path.join(__dirname,"../public");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -11,6 +12,6 @@ app.use(express.static(publicPath));
 //console.log(__dirname+"/../public");
 //console.log(publicPath);
 
-app.listen(3000,()=>{
-    console.log("listening on 3000");    
+app.listen(port,()=>{
+    console.log(`listening on ${port}`);    
 });
